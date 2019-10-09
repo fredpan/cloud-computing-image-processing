6c46b2b18fae5b301d3d6b4d6498df8eef88f1b0
fredpan <fredpan0821@gmail.com>
2019-10-09 14:53:50 -0400

    added UI

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/index.html')
 def hello_world():
-    return 'Hello World!'
+    return render_template("index.html")
+
+
+@app.route('/')
+def hello_world56():
+    return render_template("index.html")
+
+
+@app.route('/generic.html')
+def hello_world1():
+    return render_template("generic.html")
+
+@app.route('/gallery.html')
+def hello_world2():
+    return render_template("gallery.html")
 
 
 if __name__ == '__main__':
