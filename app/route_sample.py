126c2741aa0e29e1e7ef566330583b7381b46b99
fredpan <fredpan0821@gmail.com>
2019-10-09 16:13:31 -0400

    init project struc

--- app/route_sample.py
+++ app/route_sample.py
@@ -1,27 +1,23 @@
 from flask import Flask
 from flask import render_template
+from app import webapp
 
-app = Flask(__name__)
 
-
-@app.route('/index.html')
+@webapp.route('/index.html')
 def hello_world():
     return render_template("index.html")
 
 
-@app.route('/')
+@webapp.route('/')
 def hello_world56():
     return render_template("index.html")
 
 
-@app.route('/upload_management.html')
+@webapp.route('/upload_management.html')
 def hello_world1():
     return render_template("upload_management.html")
 
-@app.route('/file_management.html')
+
+@webapp.route('/file_management.html')
 def hello_world2():
     return render_template("file_management.html")
-
-
-if __name__ == '__main__':
-    app.run()
