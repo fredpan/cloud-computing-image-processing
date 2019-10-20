1e7da55e324b62c6f7358cb36b4922ad69cfeab8
fredpan <fredpan0821@gmail.com>
2019-10-20 15:19:53 -0400

    Changed the session to permanent

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, session
 
 webapp = Flask(__name__)
 webapp.secret_key = '\x80\xa9s*\x12\xc7x\xa9d\x1f(\x03\xbeHJ:\x9f\xf0!\xb1a\xaa\x0f\xee'
@@ -10,4 +10,5 @@ from app.api import apis
 
 # set the max size of img allowed
 webapp.config['MAX_CONTENT_LENGTH'] = 1024*1024*10
+session.permanent = True
 
