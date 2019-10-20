aa3767e3ca0ed58781e454fd85a81ec4f3055f20
fredpan <fredpan0821@gmail.com>
2019-10-20 17:28:56 -0400

    testing

--- app/opencv/opencv.py
+++ app/opencv/opencv.py
@@ -6,81 +6,80 @@ import numpy as np
 import argparse
 import time
 import cv2
-
-#for test
-#folderPath = "/home/yixiao/Desktop/img_database/"
-#imgName = "IMG-2019-Slider-01.jpg"
-#saveImgName = "s_IMG-2019-Slider-01.jpg"
+from . import opencv
 
 
-def imageProcess(folderPath,imgName,saveImgName):
-
-	image = cv2.imread(folderPath + imgName)
-	originalImgCopy = image.copy()
-	(H, W) = image.shape[:2]
-	(newW, newH) = (320, 320)
-	rW = W / float(newW)
-	rH = H / float(newH)
-	image = cv2.resize(image, (newW, newH))
-	(H, W) = image.shape[:2]
-
-	layerNames = [
-		"feature_fusion/Conv_7/Sigmoid",
-		"feature_fusion/concat_3"]
-
-	print("[INFO] loading EAST text detector...")
-	net = cv2.dnn.readNet("frozen_east_text_detection.pb")
-
-	blob = cv2.dnn.blobFromImage(image, 1.0, (W, H),
-		(123.68, 116.78, 103.94), swapRB=True, crop=False)
-	start = time.time()
-	net.setInput(blob)
-	(scores, geometry) = net.forward(layerNames)
-	end = time.time()
-
-	print("[INFO] text detection took {:.6f} seconds".format(end - start))
-
-	(numRows, numCols) = scores.shape[2:4]
-	rects = []
-	confidences = []
+# for test
+# folderPath = "/home/yixiao/Desktop/img_database/"
+# imgName = "IMG-2019-Slider-01.jpg"
+# saveImgName = "s_IMG-2019-Slider-01.jpg"
 
 
-	for y in range(0, numRows):
-		scoresData = scores[0, 0, y]
-		xData0 = geometry[0, 0, y]
-		xData1 = geometry[0, 1, y]
-		xData2 = geometry[0, 2, y]
-		xData3 = geometry[0, 3, y]
-		anglesData = geometry[0, 4, y]
-		for x in range(0, numCols):
-			if scoresData[x] < 0.5:
-				continue
-			(offsetX, offsetY) = (x * 4.0, y * 4.0)
-			angle = anglesData[x]
-			cos = np.cos(angle)
-			sin = np.sin(angle)
-			h = xData0[x] + xData2[x]
-			w = xData1[x] + xData3[x]
-			endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
-			endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
-			startX = int(endX - w)
-			startY = int(endY - h)
-			rects.append((startX, startY, endX, endY))
-			confidences.append(scoresData[x])
-	boxes = non_max_suppression(np.array(rects), probs=confidences)
+def imageProcess(folderPath, imgName, saveImgName):
+    image = cv2.imread(folderPath + imgName)
+    originalImgCopy = image.copy()
+    (H, W) = image.shape[:2]
+    (newW, newH) = (320, 320)
+    rW = W / float(newW)
+    rH = H / float(newH)
+    image = cv2.resize(image, (newW, newH))
+    (H, W) = image.shape[:2]
 
+    layerNames = [
+        "feature_fusion/Conv_7/Sigmoid",
+        "feature_fusion/concat_3"]
 
-	for (startX, startY, endX, endY) in boxes:
-		startX = int(startX * rW)
-		startY = int(startY * rH)
-		endX = int(endX * rW)
-		endY = int(endY * rH)
-		cv2.rectangle(originalImgCopy, (startX, startY), (endX, endY), (0, 255, 0), 2)
+    print("[INFO] loading EAST text detector...")
+    net = cv2.dnn.readNet("/var/lib/jenkins/workspace/ece1779-image-processing/app/opencv/frozen_east_text_detection.pb")
 
-	#save the processed img
-	cv2.imwrite(folderPath + saveImgName, originalImgCopy)
+    blob = cv2.dnn.blobFromImage(image, 1.0, (W, H),
+                                 (123.68, 116.78, 103.94), swapRB=True, crop=False)
+    start = time.time()
+    net.setInput(blob)
+    (scores, geometry) = net.forward(layerNames)
+    end = time.time()
 
-	return True
+    print("[INFO] text detection took {:.6f} seconds".format(end - start))
 
-#for test
-#print(imageProcess(folderPath,imgName,saveImgName))
+    (numRows, numCols) = scores.shape[2:4]
+    rects = []
+    confidences = []
+
+    for y in range(0, numRows):
+        scoresData = scores[0, 0, y]
+        xData0 = geometry[0, 0, y]
+        xData1 = geometry[0, 1, y]
+        xData2 = geometry[0, 2, y]
+        xData3 = geometry[0, 3, y]
+        anglesData = geometry[0, 4, y]
+        for x in range(0, numCols):
+            if scoresData[x] < 0.5:
+                continue
+            (offsetX, offsetY) = (x * 4.0, y * 4.0)
+            angle = anglesData[x]
+            cos = np.cos(angle)
+            sin = np.sin(angle)
+            h = xData0[x] + xData2[x]
+            w = xData1[x] + xData3[x]
+            endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
+            endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
+            startX = int(endX - w)
+            startY = int(endY - h)
+            rects.append((startX, startY, endX, endY))
+            confidences.append(scoresData[x])
+    boxes = non_max_suppression(np.array(rects), probs=confidences)
+
+    for (startX, startY, endX, endY) in boxes:
+        startX = int(startX * rW)
+        startY = int(startY * rH)
+        endX = int(endX * rW)
+        endY = int(endY * rH)
+        cv2.rectangle(originalImgCopy, (startX, startY), (endX, endY), (0, 255, 0), 2)
+
+    # save the processed img
+    cv2.imwrite(folderPath + saveImgName, originalImgCopy)
+
+    return True
+
+# for test
+# print(imageProcess(folderPath,imgName,saveImgName))
