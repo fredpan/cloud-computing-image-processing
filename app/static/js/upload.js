e7888fc5dab1d0f30ce7e0cecc6d7784e17a40d2
fredpan <fredpan0821@gmail.com>
2019-10-20 22:16:44 -0400

    Added file size check in front end

---
+++ app/static/js/upload.js
@@ -0,0 +1,12 @@
+var submitbtn;
+
+window.addEventListener("load", event=>{
+    submitbtn = document.getElementById("upload_file_btn");
+    submitbtn.addEventListener("click", ev => {
+        ev.preventDefault();
+        var size = document.getElementById("upload_file").files[0].size;
+        if (size >= 1024*1024*5){
+            alert("File too big!")
+        }
+    }, false);
+}, false);
