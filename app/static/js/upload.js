7786ed325833c16d19eaddc05a3562c17590b0b8
fredpan <fredpan0821@gmail.com>
2019-10-20 23:42:11 -0400

    bug fixed

--- app/static/js/upload.js
+++ app/static/js/upload.js
@@ -3,10 +3,12 @@ var submitbtn;
 window.addEventListener("load", event=>{
     submitbtn = document.getElementById("upload_file_btn");
     submitbtn.addEventListener("click", ev => {
-        ev.preventDefault();
         var size = document.getElementById("upload_file").files[0].size;
         if (size >= 1024*1024*5){
+            ev.preventDefault();
             alert("File too big!")
+        } else {
+            return true
         }
     }, false);
 }, false);
