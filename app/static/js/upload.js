e2498bd1c2d987cd0f11a64023858922363fdadb
hongyixiao <felixhong@live.com>
2019-10-21 21:36:10 -0400

    Final Commit 2

--- app/static/js/upload.js
+++ app/static/js/upload.js
@@ -4,9 +4,9 @@ window.addEventListener("load", event=>{
     submitbtn = document.getElementById("upload_file_btn");
     submitbtn.addEventListener("click", ev => {
         var size = document.getElementById("upload_file").files[0].size;
-        if (size >= 1024*1024*2){
+        if (size >= 1024*1024*5){
             ev.preventDefault();
-            alert("File too big!")
+            alert("File too big! Can not larger than 5MB")
         } else {
             return true
         }
